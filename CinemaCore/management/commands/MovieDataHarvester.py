16c6e082d5f60fe3b3c94375087ee8a14502c4b8
Federico <federico.alberti.gurnerio@gmail.com>
2018-09-19 14:59:26 -0300

    Finished movie data harvester

--- CinemaCore/management/commands/MovieDataHarvester.py
+++ CinemaCore/management/commands/MovieDataHarvester.py
@@ -2,8 +2,7 @@ import json
 from datetime import timedelta, datetime
 
 import requests
-from CinemaCore.models import Movie
-from CinemaCore.models import Actor
+from CinemaCore.models import Movie, Actor, MovieCrew
 
 
 # def prepare_url_to_consume(api_key, result_page):
@@ -19,11 +18,18 @@ def harvest_data_from_themoviedb():
         print(json_movies)
         actors = Actor.objects.all()
         for movie in json_movies['results']:
-            inst_movie = Movie.objects.create(name=movie['title'], moviedb_id=movie["id"], duration=timedelta(hours=2, minutes=30),
-                                 rating=movie['vote_average'],
-                                 release_date=datetime.strptime(movie['release_date'], '%Y-%m-%d'))
-            inst_movie.actors.set(actors)
+            inst_movie = Movie.objects.create(name=movie['title'], moviedb_id=movie["id"],
+                                              duration=timedelta(hours=2, minutes=30),
+                                              rating=movie['vote_average'],
+                                              release_date=datetime.strptime(movie['release_date'], '%Y-%m-%d'))
+            # inst_movie.actors.set(actors)
+            for actor in actors:
+                movie_crew = MovieCrew.objects.create(movie=inst_movie, actor=actor, was_principal_actor=True,
+                                                      role_played_description='did something')
+                movie_crew.save()
+
             inst_movie.save()
+
             # name = models.CharField(max_length=40)
             # moviedb_id = models.IntegerField(editable=False, unique=True, db_index=True)
             # duration = models.DurationField()
